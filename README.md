57cbd3f046e7abb1c352c9011d6b36b1341742cc
Aaron Filson <aaronfilson@gmail.com>
2015-09-30 14:06:53 -0700

    Updated the flow of the game to include immediate feedback on the user's input. There is now a section of if/else after each question, which evaluates the input and generates an alert as feedback.

---
+++ README.md
@@ -0,0 +1,2 @@
+<!-- Readme file for the Guessing Game assignment for B34 day 3. -->
+This program runs a simple guessing game with feedback. It has three questions, including individual question feedback. The game can be re-run by reloading the index.html webpage.
--- game.js
+++ game.js
@@ -1,50 +1,84 @@
+//game.js
+//Written by Aaron Filson, aaronfilson@gmail.com
+//Day 3 lab assignment for B34 Bootcamp at Code Fellows Seattle
+//September 30th, 2015
 
-var username = prompt('What is your name?');
-
-console.log('User entered ' + username + 'as their user name.');
-
-alert('Look at me ' + username + '! \n Do you like guessing games? I guess you do...');
-
-var qu1 = prompt('Do I have a Minor in English? Please answer yes or no.');
-
-console.log('user entered ' + qu1 + ' for qu1 variable.');
-
-var qu2 = prompt('Am I a football player? Please answer yes or no.');
-
-console.log('user entered ' + qu2 + ' for qu2 variable.');
-
-var qu3 = prompt('Did I grow up in Auburn? Please answer yes or no.');
-
-console.log('user entered ' + qu3 + ' for qu3 variable.');
+//Declaring variables
+var username;
+var qu1;
+var qu2;
+var qu3;
 
 var resQ1 = true;
 var resQ2 = true;
 var resQ3 = true;
 
-qu1 = qu1.toUpperCase();
-qu2 = qu2.toUpperCase();
-qu3 = qu3.toUpperCase();
-
-
-if ((qu1=='YES') || (qu1=='Y'))
-   {resQ1=true;}
- else
-  {resQ1=false;};
-if ((qu2=='NO')||(qu2=='N'))
-    {resQ2=true;}
-  else
-    {resQ2=false;};
-if ((qu3=='YES')||(qu3=='Y'))
-  {resQ3=true;}
-  else
-    {resQ3=false;};
-
-
-console.log('logic results: ' + resQ1 + ', ' + resQ2 + ', ' + resQ3);
-
 var textFeedbackq1 = 'Incorrect';
 var textFeedbackq2 = 'Incorrect';
 var textFeedbackq3 = 'Incorrect';
+
+//end of variables declaration
+
+username = prompt('What is your name?');
+
+console.log('User entered ' + username + ' as their user name.');
+
+alert('Nice to meet you ' + username + '! \n Do you like guessing games? I guess you do...');
+
+qu1 = prompt('Do I have a Minor in English? Please answer yes or no.');
+console.log('user entered ' + qu1 + ' for qu1 variable.');
+qu1 = qu1.toUpperCase();
+
+if ((qu1=='YES') || (qu1=='Y'))
+   {
+     resQ1=true;
+     alert('Yes, that is correct ' + username + ', I do have a Minor in English, from UPS.')
+   }
+ else
+  {
+    resQ1=false;
+    alert('No, that is not correct ' + username + ', I do have a Minor in English, from UPS.')
+  }
+
+
+
+
+qu2 = prompt('Am I a football player? Please answer yes or no.');
+console.log('user entered ' + qu2 + ' for qu2 variable.');
+qu2 = qu2.toUpperCase();
+
+if ((qu2=='NO')||(qu2=='N'))
+    {
+      resQ2=true;
+      alert('Yes, that is correct ' + username +
+       ', I do not play football. But I do cheer for the Hawks. GO SEAHAWKS!!');
+    }
+  else
+    {
+      resQ2=false;
+      alert('No, that is not correct ' + username +
+       ', I do not play football. But I do cheer for the Hawks. GO SEAHAWKS!!');
+
+    }
+
+qu3 = prompt('Did I grow up in Auburn? Please answer yes or no.');
+console.log('user entered ' + qu3 + ' for qu3 variable.');
+qu3 = qu3.toUpperCase();
+
+if ((qu3=='YES')||(qu3=='Y'))
+  {
+    resQ3=true;
+    alert('Yes, ' + username + ', I grew up in Auburn and graduated from AHS in 1997.');
+  }
+  else
+    {
+      resQ3=false;
+      alert('No, that is not correct ' + username +
+        ', I grew up in Auburn and graduated from AHS in 1997.');
+    }
+
+
+console.log('logic results: ' + resQ1 + ', ' + resQ2 + ', ' + resQ3);
 
 if (resQ1) {textFeedbackq1 = 'Correct!';}
 if (resQ2) {textFeedbackq2 = 'Correct!';}
