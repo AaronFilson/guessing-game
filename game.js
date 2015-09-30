56da5cf6969065c75c4d6b0910451ce9b6d72e05
Aaron Filson <aaronfilson@gmail.com>
2015-09-30 13:14:15 -0700

    First commit of the guessing game assignment, to load the initial version of index.html and game.js.

---
+++ game.js
@@ -0,0 +1,58 @@
+
+var username = prompt('What is your name?');
+
+console.log('User entered ' + username + 'as their user name.');
+
+alert('Look at me ' + username + '! \n Do you like guessing games? I guess you do...');
+
+var qu1 = prompt('Do I have a Minor in English? Please answer yes or no.');
+
+console.log('user entered ' + qu1 + ' for qu1 variable.');
+
+var qu2 = prompt('Am I a football player? Please answer yes or no.');
+
+console.log('user entered ' + qu2 + ' for qu2 variable.');
+
+var qu3 = prompt('Did I grow up in Auburn? Please answer yes or no.');
+
+console.log('user entered ' + qu3 + ' for qu3 variable.');
+
+var resQ1 = true;
+var resQ2 = true;
+var resQ3 = true;
+
+qu1 = qu1.toUpperCase();
+qu2 = qu2.toUpperCase();
+qu3 = qu3.toUpperCase();
+
+
+if ((qu1=='yes') || (qu1=='y') || (qu1=='Yes') || (qu1=='Y'))
+   {resQ1=true;}
+ else
+  {resQ1=false;};
+if ((qu2=='no')||(qu2=='n')||(qu2=='No')||(qu2=='N'))
+    {resQ2=true;}
+  else
+    {resQ2=false;};
+if ((qu3=='yes')||(qu3=='y')||(qu3=='Yes')||(qu3=='Y'))
+  {resQ3=true;}
+  else
+    {resQ3=false;};
+
+
+console.log('logic results: ' + resQ1 + ', ' + resQ2 + ', ' + resQ3);
+
+var textFeedbackq1 = 'Incorrect';
+var textFeedbackq2 = 'Incorrect';
+var textFeedbackq3 = 'Incorrect';
+
+if (resQ1) {textFeedbackq1 = 'Correct!';}
+if (resQ2) {textFeedbackq2 = 'Correct!';}
+if (resQ3) {textFeedbackq3 = 'Correct!';}
+
+alert('Thanks for playing my game, ' + username +
+  '. The results are ... \n  Q1: ' + textFeedbackq1 + '\n Q2: '
+  + textFeedbackq2 + '\n Q3: ' + textFeedbackq3 + ' \n \nPlease reload to play again.'
+  );
+
+//EOF
