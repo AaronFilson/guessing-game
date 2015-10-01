d55ea111e2c0352e0f2e1e21f72c68d0cb36f0e7
Aaron Filson <aaronfilson@gmail.com>
2015-09-30 22:11:00 -0700

    Updated style of code, altered formatting of readme. Last update for September!

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 <!-- Readme file for the Guessing Game assignment for B34 day 3. -->
 This program runs a simple guessing game with feedback. It has three questions, including individual question feedback. The game can be re-run by reloading the index.html webpage.
 
-There is limited error correction for user input. At the end the user is presented with a summary.
+  There is limited error correction for user input. At the end the user is presented with a summary.
 
-To run this program, use a HTML5 and JavaScript 5 compliant web browser to open index.html.
+  To run this program, use a HTML5 and JavaScript 5 compliant web browser to open index.html.
 
-Enjoy!
+  Enjoy!
 
--Aaron Filson
+  -Aaron Filson
--- game.js
+++ game.js
@@ -22,57 +22,72 @@ var countingRightAnswers = 0;
 
 //end of variables declaration
 
+//Get user's name via prompt and store in username variable.
 username = prompt('What is your name?');
 
 console.log('User entered ' + username + ' as their user name.');
 
 alert('Nice to meet you ' + username + '! \n Do you like guessing games? I guess you do...');
 
+//Question 1
 qu1 = prompt('Do I have a Minor in English? Please answer yes or no.');
-console.log('user entered ' + qu1 + ' for qu1 variable.');
+console.log('User entered ' + qu1 + ' for qu1 variable.');
 qu1 = qu1.toUpperCase();
 
 if ((qu1=='YES') || (qu1=='Y')) {
-     resQ1=true;
-     alert('Yes, that is correct ' + username + ', I do have a Minor in English, from UPS.');
-     countingRightAnswers++;
-   } else {
-    resQ1=false;
-    alert('No, that is not correct ' + username + ', I do have a Minor in English, from UPS.');}
+  resQ1=true;
+  alert('Yes, that is correct ' + username + ', I do have a Minor in English, from UPS.');
+  countingRightAnswers++;
+} else {
+  resQ1=false;
+  alert('No, that is not correct ' + username + ', I do have a Minor in English, from UPS.');
+}
 
+//Question 2
 qu2 = prompt('Am I a football player? Please answer yes or no.');
-console.log('user entered ' + qu2 + ' for qu2 variable.');
+console.log('User entered ' + qu2 + ' for qu2 variable.');
 qu2 = qu2.toUpperCase();
 
 if ((qu2=='NO')||(qu2=='N')) {
-      resQ2=true;
-      alert('Yes, that is correct ' + username +
-       ', I do not play football. But I do cheer for the Hawks. GO SEAHAWKS!!');
-      countingRightAnswers++;
-    } else {
-      resQ2=false;
-      alert('No, that is not correct ' + username +
-       ', I do not play football. But I do cheer for the Hawks. GO SEAHAWKS!!');}
+  resQ2=true;
+  alert('Yes, that is correct ' + username +
+  ', I do not play football. But I do cheer for the Hawks. GO SEAHAWKS!!');
+  countingRightAnswers++;
+} else {
+  resQ2=false;
+  alert('No, that is not correct ' + username +
+  ', I do not play football. But I do cheer for the Hawks. GO SEAHAWKS!!');
+}
 
+//Question 3
 qu3 = prompt('Did I grow up in Auburn? Please answer yes or no.');
-console.log('user entered ' + qu3 + ' for qu3 variable.');
+console.log('User entered ' + qu3 + ' for qu3 variable.');
 qu3 = qu3.toUpperCase();
 
 if ((qu3=='YES')||(qu3=='Y')) {
-    resQ3=true;
-    alert('Yes, ' + username + ', I grew up in Auburn and graduated from AHS in 1997.');
-    countingRightAnswers++;
-  } else {
-      resQ3=false;
-      alert('No, that is not correct ' + username +
-        ', I grew up in Auburn and graduated from AHS in 1997.');}
+  resQ3=true;
+  alert('Yes, ' + username + ', I grew up in Auburn and graduated from AHS in 1997.');
+  countingRightAnswers++;
+} else {
+  resQ3=false;
+  alert('No, that is not correct ' + username +
+  ', I grew up in Auburn and graduated from AHS in 1997.');
+}
 
+//Logic section, resQ1 etc are initialized to Incorrect.
 console.log('logic results: ' + resQ1 + ', ' + resQ2 + ', ' + resQ3);
 
-if (resQ1) {textFeedbackq1 = 'Correct!';}
-if (resQ2) {textFeedbackq2 = 'Correct!';}
-if (resQ3) {textFeedbackq3 = 'Correct!';}
+if (resQ1) {
+  textFeedbackq1 = 'Correct!';
+}
+if (resQ2) {
+  textFeedbackq2 = 'Correct!';
+}
+if (resQ3) {
+  textFeedbackq3 = 'Correct!';
+}
 
+//Summary display for user
 alert('Thanks for playing my game, ' + username +
   '. The results are ... \n\nQ1: ' + textFeedbackq1 + '\nQ2: '
   + textFeedbackq2 + '\nQ3: ' + textFeedbackq3 +
